f8fe7cc5b9f5b4c030a8b65381dd4409893d722d
ravishanigarapu <133210792+ravishanigarapu@users.noreply.github.com>
2025-04-07 17:58:50 +0530

    AMM-1168 (#64)
    * AMM-1168
    
    * Update application.properties

--- src/main/java/com/iemr/mmu/service/tele_consultation/TeleConsultationServiceImpl.java
+++ src/main/java/com/iemr/mmu/service/tele_consultation/TeleConsultationServiceImpl.java
@@ -74,10 +74,14 @@ public class TeleConsultationServiceImpl implements TeleConsultationService {
 	private TCRequestModelRepo tCRequestModelRepo;
 	@Autowired
 	private BeneficiaryFlowStatusRepo beneficiaryFlowStatusRepo;
-	@Autowired
 	private CommonDoctorServiceImpl commonDoctorServiceImpl;
 	@Autowired
+	public void setCommonDoctorServiceImpl(CommonDoctorServiceImpl commonDoctorServiceImpl) {
+		this.commonDoctorServiceImpl = commonDoctorServiceImpl;
+	}
+  @Autowired
 	private CookieUtil cookieUtil;
+
 
 	public int createTCRequest(TCRequestModel tCRequestModel) {
 		TCRequestModel tCRequestModelRS = tCRequestModelRepo.save(tCRequestModel);
